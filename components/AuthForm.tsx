7e85e625bbc98b4c50fd9ce4df5a14e71a87aa79
DuRg3sH-8H <durgeshbhandari834@gmail.com>
2024-08-08 15:11:27 +0545

    backend work

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,8 +20,11 @@ import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -33,10 +36,28 @@ const AuthForm = ({ type }: { type: string }) => {
       password: "",
     },
   });
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async(data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-    console.log(values);
-    setIsLoading(false);
+    try {
+      //signup with appwrite ans create plaid token
+      if(type=== 'sign-up'){
+        const newUser = await signUp(data);
+        setUser(newUser);
+      }
+
+      if(type === 'sign-in'){
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+
+        if(response.success) router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    }finally {
+      setIsLoading(false);
+    }
   }
 
   return (
@@ -92,6 +113,12 @@ const AuthForm = ({ type }: { type: string }) => {
                     name="address1"
                     label="Address"
                     placeholder="Enter your specific address"
+                  />
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your City Name"
                   />
                   <div className="flex gap-4">
                     <CustomInput
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,46 @@
+'use server'
+
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../apppwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
+export const signIn = async () => {
+  try {
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+export const signUp = async (userData : SignUpParams) => {
+  const { firstName, lastName, email, password } = userData;
+  try {
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(ID.unique(),
+    email, 
+    password, 
+    `${firstName} ${lastName}`);
+    const session = await account.createEmailPasswordSession(email, password);
+  
+    cookies().set("my-custom-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount)
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
